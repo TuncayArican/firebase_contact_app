301cbe550521dfdf8cc3e151985dd64759ed4cc9
TuncayArican <tuncayarican07@gmail.com>
2022-04-19 00:24:52 +0300

    Ununsed functions have been deleted

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,5 @@
 // Database bilgi ekleme,bilgiyi alma, bilgi silme ve değiştirme
-import firebase from "./firebase"
+
 import {useState,useEffect} from "react";
 import { getDatabase,ref,set,push,onValue, remove,update} from "firebase/database";
 import Toastify from "./toast";
@@ -7,7 +7,7 @@ import Toastify from "./toast";
 // Bilgi Ekleme
 export const AddUser=(info)=>{
     const db = getDatabase();
-    const userRef=ref(db,"baglanti");
+    const userRef=ref(db,"customer");
     const newUserRef=push(userRef)
     set((newUserRef),{
         username:info.username,
@@ -26,7 +26,7 @@ export const useFetch=()=>{
         setIsLoading(true)
 
         const db = getDatabase();
-        const userRef=ref(db,"baglanti");
+        const userRef=ref(db,"customer");
 
         onValue(userRef, (snapshot) => {
             const data = snapshot.val();
@@ -45,8 +45,8 @@ export const useFetch=()=>{
 // Bilgi silme
 export const DeleteUser=(id)=>{
         const db = getDatabase();
-        const userRef=ref(db,"baglanti");
-        remove(ref(db,"baglanti/"+id))
+
+        remove(ref(db,"customer/"+id))
 
         Toastify("Kullanıcı bilgisi silindi")
 }
@@ -57,7 +57,7 @@ export const EditUser=(info)=>{
     const db = getDatabase();
     const updates = {};
 
-    updates["baglanti/"+info.id]=info;
+    updates["customer/"+info.id]=info;
     return update(ref(db),updates);
 
 }
